e83d6c40b51c3472caf208a680eeb49ba6d65e35
Tomáš Figura <tomas.figura@post.cz>
2017-12-17 15:51:35 +0100

    Added missing HTML, removed inline Javascript and fixed formating

--- index.php
+++ index.php
@@ -1,43 +1,33 @@
-<script type="text/javascript" src="jquery.min.js"></script>
-<script type="text/javascript" src="jquery.form.js"></script>
-<script type="text/javascript">
-$(document).ready(function()
-{
-
-$('body').on('change','#photoimg', function()
- {
-var A=$("#imageloadstatus");
-var B=$("#imageloadbutton");
-
-$("#imageform").ajaxForm({target: '#preview',
-beforeSubmit:function(){
-A.show();
-B.hide();
-},
-success:function(){
-A.hide();
-B.show();
-},
-error:function(){
-A.hide();
-B.show();
-} }).submit();
-});
-
-});
-</script>
 <?php
-include('db.php');
-session_start();
-$session_id='1';
+  include('db.php');
+  session_start();
+  $session_id = '1';
 ?>
-<div id='preview'>
-</div>
-<form id="imageform" method="post" enctype="multipart/form-data" action='ajaximage.php'>
-Upload image:
-<div id='imageloadstatus' style='display:none'><img src="loader.gif" alt="Uploading...."/></div>
-<div id='imageloadbutton'>
-<input type="file" name="photoimg" id="photoimg" />
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Image uploading and resizing site</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-</div>
-</form>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script type="text/javascript" src="jquery.min.js"></script>
+        <script type="text/javascript" src="jquery.form.js"></script>
+    </head>
+    <body>
+        <div id='preview'>
+        </div>
+        <form id="imageform" method="post" enctype="multipart/form-data" action='ajaximage.php'>
+        Upload image:
+        <div id='imageloadstatus' style='display:none'><img src="loader.gif" alt="Uploading...."/></div>
+        <div id='imageloadbutton'>
+        <input type="file" name="photoimg" id="photoimg" />
+        </div>
+        </form>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
