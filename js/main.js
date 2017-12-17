f9e60e5bdb1c6e69fae6fe1aa62c13cb03b07575
Tomáš Figura <tomas.figura@post.cz>
2017-12-17 17:22:11 +0100

    Added function swapVisibility

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
   $('body').on('change','#photoimg', function(){
-  var A = $("#imageloadstatus");
-  var B = $("#imageloadbutton");
+    var imgLoadStatus = $("#imageloadstatus");
+    var imgLoadBtn = $("#imageloadbutton");
 
-  $("#imageform").ajaxForm({target: '#preview',
-    beforeSubmit:function(){
-      A.show();
-      B.hide();
-    },
-    success:function(){
-      A.hide();
-      B.show();
-    },
-    error:function(){
-      A.hide();
-      B.show();
-    } }).submit();
-  });
+    $("#imageform").ajaxForm({target: '#preview',
+      beforeSubmit:function(){
+        swapVisibility(imgLoadStatus, imgLoadBtn);
+      },
+      success:function(){
+        swapVisibility(imgLoadStatus, imgLoadBtn);
+      },
+      error:function(){
+        swapVisibility(imgLoadStatus, imgLoadBtn);
+      } }).submit();
+    });
 });
+
+function swapVisibility(a, b){
+  a.show();
+  b.hide();
+}
