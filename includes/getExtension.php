202e80acd6e84ecca773cc32e4c7a64ce0c43540
Tomáš Figura <tomas.figura@post.cz>
2017-12-17 16:24:32 +0100

    Improved readabilty and removed useless rows

--- includes/getExtension.php
+++ includes/getExtension.php
@@ -1,12 +1,11 @@
 <?php
 function getExtension($str)
 {
-$i = strrpos($str,".");
-if (!$i)
-{
-return "";
-}
-$l = strlen($str) - $i;
-$ext = substr($str,$i+1,$l);
-return $ext;
+  $dotPosition = strrpos($str,".");
+  if (!$dotPosition)
+  {
+    return "";
+  }
+  $extensionLenght = strlen($str) - $dotPosition;
+  return substr($str,$dotPosition + 1, $extensionLenght);
 }
