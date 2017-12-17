34b588bcc628676083f5fb9da82383178684a7c4
Tomáš Figura <tomas.figura@post.cz>
2017-12-17 17:50:26 +0100

    If table doesnt exits create it and fill it with users

---
+++ usersTable.php
@@ -0,0 +1,47 @@
+<?php
+include('db.php');
+
+function tableExists($pdo, $table) // Function checks if table already exists
+{
+  try
+  {
+    $result = $pdo->query("SELECT 1 FROM $table LIMIT 1");
+  }
+  catch (Exception $e)
+  {
+    return FALSE;
+  }
+  return $result !== FALSE;
+}
+
+if(tableExists($db, "users") == FALSE) // I table doesnt exist create it
+{
+  $sql = file_get_contents('users.sql');
+
+  if ($db->query($sql) === TRUE) {
+      echo "Table users created successfully";
+
+      $users = array(
+        array("tomas", "12345", null),
+        array("andrej", "123456", null),
+        array("test", "123457", null),
+        array("lojzo", "123458", null)
+      );
+
+      foreach ($users as $user) {
+        try {
+          $stmt = $dbh->prepare("INSERT INTO users (username, password, profile_image) VALUES (:username, :password, :profile_image)");
+          $stmt->bindParam(':username', $user[0]);
+          $stmt->bindParam(':password', $user[1]);
+          $stmt->bindParam(':profile_image', $user[2]);
+          $stmt->execute();
+        } catch (\Exception $e) {
+          echo $e->getMessage();
+        }
+
+      }
+  } else {
+      echo "Error creating table: " . $db->error;
+  }
+  $db->close();
+}
