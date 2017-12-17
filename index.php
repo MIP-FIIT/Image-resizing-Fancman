fe78c9a2dff5472a0c80b85718ce93de897151aa
Tomáš Figura <tomas.figura@post.cz>
2017-12-17 15:57:24 +0100

    Added normalize.css to normalize page look on every browser

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
 
         <script type="text/javascript" src="jquery.min.js"></script>
         <script type="text/javascript" src="jquery.form.js"></script>
